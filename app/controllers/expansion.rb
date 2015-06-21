1d4dc7c66c81f30f0e533cb9f76b3866b72d0017
soba1104 <shiba@rvm.jp>
2015-06-21 12:43:24 +0900

    セキュリティ的にまずい箇所にTODOコメントを追加

--- app/controllers/expansion.rb
+++ app/controllers/expansion.rb
@@ -3,6 +3,7 @@ Mtgtv::App.controllers :expansion do
     name = params[:name].upcase
     path = Padrino.root(Config[:data], "#{name}.json")
     # TODO json の存在チェックとエラー処理
+    # TODO 相対パスを入れられても大丈夫なようにエスケープ、ないし候補のエキスパンションと照合する。
     json = File.read(path)
     @expansion = Expansion.from_json(File.read(path))
     puts @expansion.cards.inspect
