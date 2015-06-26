130aa3284c225de2ab029804fb27db2c12b1287c
soba1104 <shiba@rvm.jp>
2015-06-26 19:39:27 +0900

    不要な処理を削除

--- app/controllers/expansion.rb
+++ app/controllers/expansion.rb
@@ -1,40 +1,15 @@
 Mtgtv::App.controllers :expansion do
-  loader = proc do |name|
-    # TODO json の存在チェックとエラー処理
-    # TODO 相対パスを入れられても大丈夫なようにエスケープ、ないし候補のエキスパンションと照合する。
-    name = name.upcase
-    path = Padrino.root(Config[:data], "#{name}.json")
-    File.read(path)
-  end
-
   get :index, :with => :name do
-    json = loader.call(params[:name])
-    @expansion = Expansion.from_json(json)
+    # TODO expansion の名前が正しいかどうかのチェック
+    @name = params[:name].upcase
     render 'expansion/index'
   end
 
   get :json, :map => '/expansion/:name/json', :provides => :json do
-    loader.call(params[:name])
+    # TODO json の存在チェックとエラー処理
+    # TODO 相対パスを入れられても大丈夫なようにエスケープ、ないし候補のエキスパンションと照合する。
+    name = params[:name].upcase
+    path = Padrino.root(Config[:data], "#{name}.json")
+    File.read(path)
   end
-
-  # get :index, :map => '/foo/bar' do
-  #   session[:foo] = 'bar'
-  #   render 'index'
-  # end
-
-  # get :sample, :map => '/sample/url', :provides => [:any, :js] do
-  #   case content_type
-  #     when :js then ...
-  #     else ...
-  # end
-
-  # get :foo, :with => :id do
-  #   'Maps to url '/foo/#{params[:id]}''
-  # end
-
-  # get '/example' do
-  #   'Hello world!'
-  # end
-  
-
 end
